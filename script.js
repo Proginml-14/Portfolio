202c5ce30a1739bd9667595d28352fbe9bf60b25
Md Inamul Haque <mdinamulhaque2021@gmail.com>
2025-01-27 20:24:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// const menubar = document.querySelector('#menu');
+// const Navbar = document.querySelector('.navbar');
+// menubar.onclick=()=>{
+//     menubar.classList.toggle('bx-x');
+//     Navbar.classList.toggle('active')
+// }
+// const section=document.querySelectorAll('section');
+// const navlink = document.querySelectorAll('header nav a')
+// window.onscroll = ()=>{
+//     section.forEach(sec=>{
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id')
+//         if(top>offset && top < offset + height){
+//             sec.classList.add('start-animation');
+//             navlink.forEach(links=>{
+//                 links.classList.remove('active')
+//                 document.querySelector('header nav a[href*='+id+']').classList.add('active')
+              
+//             })
+//         }
+//     })
+//     var header = document.querySelector('.header');
+//     header.classList.toggle('sticky',window.scrollY>100)
+//     menubar.classList.remove('bx-x');
+//     Navbar.classList.remove('active')
+// } 
+
+
+
+
+const menubar = document.querySelector('#menu');
+const Navbar = document.querySelector('.navbar');
+menubar.onclick=()=>{
+  menubar.classList.toggle('bx-x');
+  Navbar.classList.toggle('active')
+}
+
+const section=document.querySelectorAll('section');
+const navlink = document.querySelectorAll('header nav a')
+
+window.onscroll = ()=>{
+  section.forEach(sec=>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id')
+    if(top>offset && top < offset + height){
+      sec.classList.add('start-animation');
+      navlink.forEach(links=>{
+        links.classList.remove('active')
+        document.querySelector('header nav a[href*='+id+']').classList.add('active')
+      })
+    }
+  })
+
+  var header = document.querySelector('.header');
+  header.classList.toggle('sticky',window.scrollY>100)
+  menubar.classList.remove('bx-x');
+  Navbar.classList.remove('active')
+
+  // Arrow up button functionality
+const arrowUpButton = document.querySelector('.arrow-up');
+window.addEventListener('scroll', () => {
+  const aboutSection = document.getElementById('about');
+  const aboutSectionTop = aboutSection.offsetTop;
+  if (window.scrollY >= aboutSectionTop) {
+    arrowUpButton.style.display = 'block';
+  } else {
+    arrowUpButton.style.display = 'none';
+  }
+ });
+}
